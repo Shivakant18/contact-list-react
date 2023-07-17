4f25beb4a3ff19205d005595f0503f378f607eb9
Shiva Kant Mishra <127529918+Shivakant18@users.noreply.github.com>
2023-07-17 10:42:22 +0530

    Updates

---
+++ precache-manifest.6b100d192c1a41b0cc8ee634367f8313.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e62172c05310cc21f986510abbeb01",
+    "url": "/contact-list-react/index.html"
+  },
+  {
+    "revision": "ee331c3cb9d54da7eb3e",
+    "url": "/contact-list-react/static/css/main.933f1631.chunk.css"
+  },
+  {
+    "revision": "e573333b5bfd8075f126",
+    "url": "/contact-list-react/static/js/2.03e32f27.chunk.js"
+  },
+  {
+    "revision": "ee331c3cb9d54da7eb3e",
+    "url": "/contact-list-react/static/js/main.c4014552.chunk.js"
+  },
+  {
+    "revision": "9c8ecff3dd450428a062",
+    "url": "/contact-list-react/static/js/runtime-main.6b63bfca.js"
+  },
+  {
+    "revision": "4f4f88bc8f4a10a3751ed5ca15ddc809",
+    "url": "/contact-list-react/static/media/add-contact.4f4f88bc.svg"
+  },
+  {
+    "revision": "4abe51fd0358705bc9ace3fe3d81c802",
+    "url": "/contact-list-react/static/media/bin.4abe51fd.svg"
+  },
+  {
+    "revision": "a7d974eefbc56673274d9fb1280f7790",
+    "url": "/contact-list-react/static/media/profile.a7d974ee.svg"
+  }
+]);
